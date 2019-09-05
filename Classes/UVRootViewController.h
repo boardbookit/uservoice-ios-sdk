8b0f4514a455d53c12013da722dd40dd39f7a5bd
tcoyle <tcoyle88@gmail.com>
2019-09-05 10:20:31 -0400

    testing header flag

--- Classes/UVRootViewController.h
+++ Classes/UVRootViewController.h
@@ -15,6 +15,7 @@
 
 @property (nonatomic, retain) NSString *viewToLoad;
 @property (nonatomic, retain) UVInitialLoadManager *loader;
+@property (nonatomic, retain) NSString *testFlag;
 
 - (id)initWithViewToLoad:(NSString *)theViewToLoad;
 
